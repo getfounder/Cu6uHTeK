b6051c204ce82f1e9977c39635f2c55797ef15b6
Getfounder <getfounder@yandex.ru>
2023-03-23 19:13:24 +0400

    Version 0.0.2

--- main.py
+++ main.py
@@ -12,20 +12,40 @@ TG_Bot = telebot.TeleBot(cfg.API_KEY)
 def greeting(message):
     # Greeting
     TG_Bot.send_message(message.from_user.id, cfg.MESSAGES["greeting"])
-    TG_Bot.send_message(message.from_user.id, cfg.MESSAGES["ready"])
 
-    TG_Bot.register_next_step_handler(message, start_registration)
+    start_registration(message)
 
 
 def start_registration(message):
     # Creating Buttons
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
     buttons = []
     for answer in cfg.ANSWERS["ready"]:
         buttons += [types.KeyboardButton(answer)]
+
     markup.add(*buttons)
 
-    TG_Bot.send_message(message.from_user.id, "Заглушка!😀", reply_markup=markup)
+    # Sending Messages
+    TG_Bot.send_message(message.from_user.id, cfg.MESSAGES["ready"], reply_markup=markup)
+
+    TG_Bot.register_next_step_handler(message, choose_category)
+
+
+def choose_category(message):
+    # Creating Buttons
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    buttons = []
+    for answer in cfg.ANSWERS["category"]:
+        buttons += [types.KeyboardButton(answer)]
+
+    markup.add(*buttons)
+
+    # Sending Messages
+    TG_Bot.send_message(message.from_user.id, cfg.MESSAGES["category"], reply_markup=markup)
+
+    TG_Bot.register_next_step_handler(message, choose_category)
 
 
 if __name__ == "__main__":
