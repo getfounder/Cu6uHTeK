679e2974ce4966f7b9ee58469d6219c24e492c44
Getfounder <getfounder@yandex.ru>
2023-03-23 19:06:20 +0400

    Version 0.0.1

--- main.py
+++ main.py
@@ -10,8 +10,22 @@ TG_Bot = telebot.TeleBot(cfg.API_KEY)
 
 @TG_Bot.message_handler(commands=['start'], content_types=['text'])
 def greeting(message):
-    # Greeting & Start Registration
+    # Greeting
     TG_Bot.send_message(message.from_user.id, cfg.MESSAGES["greeting"])
+    TG_Bot.send_message(message.from_user.id, cfg.MESSAGES["ready"])
+
+    TG_Bot.register_next_step_handler(message, start_registration)
+
+
+def start_registration(message):
+    # Creating Buttons
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = []
+    for answer in cfg.ANSWERS["ready"]:
+        buttons += [types.KeyboardButton(answer)]
+    markup.add(*buttons)
+
+    TG_Bot.send_message(message.from_user.id, "Заглушка!😀", reply_markup=markup)
 
 
 if __name__ == "__main__":
