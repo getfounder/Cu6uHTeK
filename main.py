092d992a55debd159afc10707a7d3feb5bd306dd
Getfounder <getfounder@yandex.ru>
2023-03-23 22:58:39 +0400

    0.2.0

--- main.py
+++ main.py
@@ -132,11 +132,22 @@ def choose_sport(message):
     # Deleting Buttons
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup = types.ReplyKeyboardRemove()
-    # Sending Messages
+    
     if information[3] != "Волгоград":
+        # Sending Messages
         bot.send_message(message.from_user.id, cfg.MESSAGES["hotel_name"], reply_markup=markup)
         bot.register_next_step_handler(message, write_hotel_name)
     else:
+        # Creating Buttons 
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+        buttons = []
+        for city in cfg.ANSWERS["choose_guests"]:
+            buttons += [types.KeyboardButton(city)]
+
+        markup.add(*buttons)
+
+        # Sending Messages
         bot.send_message(message.from_user.id, cfg.MESSAGES["choose_guests"], reply_markup=markup)
         bot.register_next_step_handler(message, choose_guests)
 
@@ -174,10 +185,10 @@ def write_hotel_number(message):
 
 
 def choose_guests(message):
-    if message.text == "Да":
-        # Deleting Buttons 
-        markup = types.ReplyKeyboardRemove()
+    # Deleting Buttons 
+    markup = types.ReplyKeyboardRemove()
 
+    if message.text == "Да":
         # Sending Messages
         bot.send_message(message.from_user.id, cfg.MESSAGES["write_guests"], reply_markup=markup)
         bot.register_next_step_handler(message, write_guests)
@@ -197,21 +208,16 @@ def write_guests(message):
 
 
 def process_final_step(message):
-    try:
-        print(information)
-        qr_info = information[0] + '\n' + information[1]
-        print(qr_info, 'qr')
-        qrcode.make(qr_info).save(str(message.from_user.id) + '.png')
-        bot.send_message(message.from_user.id, "Ваш QR-код:")
-        bot.send_photo(message.from_user.id, open(str(message.from_user.id) + '.png', 'rb'))
-        for guest in guests_list:
-            qr_info = "Гость" + '\n' + guest
-            qrcode.make(qr_info).save(str(message.from_user.id) + guest + '.png')
-            bot.send_message(message.from_user.id, "QR-код на имя " + guest)
-            bot.send_photo(message.from_user.id, open(str(message.from_user.id) + guest + '.png', 'rb'))
+    # Deleting Buttons 
+    markup = types.ReplyKeyboardRemove()
 
-    except Exception as e:
-        print(e)
+    qr_info = f"{information[0]}\n{information[1]}\n{information[3]}"
+
+    qrcode.make(qr_info).save("temps/qrcode.png")
+
+    bot.send_message(message.from_user.id, cfg.MESSAGES["QR_code"], reply_markup=markup)
+    bot.send_photo(message.from_user.id, open("temps/qrcode.png", 'rb'))
 
 
-bot.polling(none_stop=True, interval=0)
+if __name__ == "__main__":
+    bot.polling(none_stop=True, interval=0)
