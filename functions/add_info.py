c7dbbd9426f7448b2d564432ed3de64964627421
Getfounder <getfounder@yandex.ru>
2023-03-24 13:58:40 +0400

    1.0.0 Alpha Test

--- functions/add_info.py
+++ functions/add_info.py
@@ -1,4 +1,7 @@
+import config as cfg
+
 import gspread
+from gspread_formatting import *
 
 Dict = {
     'A': 'ID',
@@ -20,18 +23,42 @@ def add_info(user_data):
         sh = gc.open('UserInfo')
     except Exception:
         gc.create("UserInfo")
+        sh.share('sibintecompany@gmail.com', perm_type='user', role='writer')
         sh = gc.open('UserInfo')
     
-    sh.share('sibintecompany@gmail.com', perm_type='user', role='writer')
+    
     
     worksheet = sh.sheet1
 
     if len(worksheet.col_values(1)) == 0:
         for key in Dict.keys():
             worksheet.update(key + '1', Dict[key])
+        formating(worksheet, 1, (164, 194, 244), True, "CENTER")
+
+        for setting in cfg.Cell_Settings:
+            set_column_width(worksheet, *setting)
 
     id = len(worksheet.col_values(1))
     user_data["A"] = id
     
     for key in user_data.keys():
-            worksheet.update(key + f'{id + 1}', user_data[key])
+        worksheet.update(key + f'{id + 1}', user_data[key])
+
+    if len(user_data["I"]) * 8 > cfg.Cell_Settings[-1][-1]:
+        set_column_width(worksheet, "I", len(user_data["I"]) * 8)
+
+    if len(user_data["C"]) * 8 > cfg.Cell_Settings[-1][-1]:
+        set_column_width(worksheet, "C", len(user_data["C"]) * 8)
+
+    formating(worksheet, id + 1, cfg.Color_Settings[user_data["B"]])
+
+
+def formating(worksheet, id, color, is_bold=False, align="LEFT"):
+
+    fmt = CellFormat(
+        backgroundColor=Color(*[x / 255 for x in color]),
+        textFormat=TextFormat(bold=is_bold),
+        horizontalAlignment=align
+        )
+
+    format_cell_range(worksheet, f'A{id}:I{id}', fmt)
